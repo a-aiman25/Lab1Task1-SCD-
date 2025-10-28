c2fd5406106d66dc82dd08f4ff81c7315dc7319b
Aiman Waseem <155463719+a-aiman25@users.noreply.github.com>
2025-10-28 22:49:20 +0500

    Update Task1.java

--- Task1.java
+++ Task1.java
@@ -2,5 +2,15 @@ package Lab1Task1;
 public class Task1 {
 	public static void main(String[] args) {
 		System.out.println("Hello! Welcome to Java.");
+
+		Scanner input = new Scanner(System.in);
+        System.out.print("Enter first number: ");
+        int num1 = input.nextInt();
+        System.out.print("Enter second number: ");
+        int num2 = input.nextInt();
+        int sum = num1 + num2;
+        System.out.println("The sum of " + num1 + " and " + num2 + " is: " + sum);
+        input.close();
 	}
 }
+
